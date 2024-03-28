1e52cb890131fa6a16272df4b53725c08905b637
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-28 15:16:43 +0700

    Add Create Project

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Projects') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
+                
+                <form action="">
+
+                    <div class="flex flex-col gap-y-5">
+                        <h1 class="text-3xl text-indigo-950 font-bold">
+                            Add New Project
+                        </h1>
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Name
+                            </h3>
+                            <input type="text" id="name" name="name" placeholder="Add Your Name">
+                        </div>
+
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Category
+                            </h3>
+                            <select name="category" id="category">
+                                <option value="">Choose Category Below</option>
+                                <option value="Website Development">Website Development</option>
+                                <option value="App Development">App Development</option>
+                                <option value="Graphic Design">Graphic Design</option>
+                                <option value="Digital Marketing">Digital Marketing</option>
+                            </select>
+                        </div>
+
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                Cover Image
+                            </h3>
+                            <input type="file" id="cover" name="cover">
+                        </div>
+
+                        <div class="flex flex-col gap-y-2">
+                            <h3>
+                                About
+                            </h3>
+                            <textarea name="about" id="about" cols="30" rows="10" placeholder="Add About"></textarea>
+                        </div>
+
+                        <button type="submit" class="py-4 w-full rounded-full bg-violet-700 font-bold text-white">
+                            Upload Project
+                        </button>
+
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
