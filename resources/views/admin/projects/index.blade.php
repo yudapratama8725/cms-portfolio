74db95b6162d97c18ea392e670aa51579e1d9eb0
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-29 22:10:13 +0700

    Add Layout Admin Page Project

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,10 +7,50 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-28">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col">
                 <a href="{{ route('admin.projects.create') }}" class="w-fit py-4 px-10 text-white bg-indigo-950 rounded-full font-bold">
                     Add New Project
                 </a>
+
+                <hr class="my-10">
+                <div class="flex flex-col gap-y-5">
+
+                    {{-- Melakukan Foreach Data Dari Table Projects --}}
+                    <div class="item-project flex flex-row items-center justify-between">
+
+                        <div class="flex flex-row items-center gap-x-5">
+                            <img src="{{ asset('images/thumbnails/thumbnail3.png') }}" class="object-cover w-[120px] h-[90px] rounded-2xl">
+                            <div class="flex flex-col gap-y-1">
+                                <h3 class="font-bold text-xl">
+                                    Portoku Creative Folio
+                                </h3>
+                                <p class="text-sm text-slate-400">
+                                    Website Development
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="flex flex-row items-center gap-x-2">
+                            <a href="#" class="py-3 px-5 rounded-full bg-indigo-950 text-white">
+                                Add Tools
+                            </a>
+                            <a href="#" class="py-3 px-5 rounded-full bg-indigo-950 text-white">
+                                Add Screenshoot
+                            </a>
+                        </div>
+
+                        <div class="flex flex-row items-center gap-x-2">
+                            <a href="#" class="py-3 px-5 rounded-full bg-indigo-500 text-white">
+                                Edit
+                            </a>
+                            <a href="#" class="py-3 px-5 rounded-full bg-red-500 text-white">
+                                Delete
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
